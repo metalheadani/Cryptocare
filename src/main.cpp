3a9b2ae1af78be4a19c86eb0c00a4a141ed929a1
Anirudha Jain <anirudhajain.910@gmail.com>
2018-11-18 22:47:35 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1087,10 +1087,10 @@ uint256 static GetOrphanRoot(const CBlockHeader* pblock)
 
 int64 static GetBlockValue(int nHeight, int64 nFees)
 {
-    int64 nSubsidy = 50 * COIN;
+    int64 nSubsidy = 47565 * COIN;
 
     // Subsidy is cut in half every 840000 blocks, which will occur approximately every 4 years
-    nSubsidy >>= (nHeight / 840000); // Cryptocare: 840k blocks in ~4 years
+    nSubsidy >>= (nHeight / 840960); // Cryptocare: 840k blocks in ~4 years
 
     return nSubsidy + nFees;
 }
